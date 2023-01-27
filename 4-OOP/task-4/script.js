e148b965e616a76dd78a66242fccfee0284368f0
AmetJunior <ametsejdaliev@gmail.com>
2023-01-27 20:38:56 +0300

    27.01.2023-OOP-task-4-last

--- 4-OOP/task-4/script.js
+++ 4-OOP/task-4/script.js
@@ -23,8 +23,28 @@ class Dictionary {
     }
 }
 
-const dictionary = new Dictionary('Java', 'Number')
-dictionary.add('Web-developer', 'this Software Enginner make a web-sites')
-dictionary.add('game-developer', 'he make a games')
-dictionary.remove('game-developer')
-dictionary.showAllWords()
+class HardWordsDictionary extends Dictionary {
+    constructor(name) {
+        super(name)
+    };
+    add(word = '', description = '') {
+        this.words[word] = {
+            word,
+            description,
+            isDifficult: true,
+        }
+    };
+}
+
+const hardWordsDictionary = new HardWordsDictionary('Сложные слова')
+hardWordsDictionary.add('дилетант', 'Тот, кто занимается наукой или искусством без специальной подготовки, обладая только поверхностными знаниями.');
+hardWordsDictionary.add('неологизм', 'Новое слово или выражение, а также новое значение старого слова.');
+hardWordsDictionary.add('квант', 'Неделимая часть какой-либо величины в физике.');
+hardWordsDictionary.remove('неологизм');
+hardWordsDictionary.showAllWords();
+
+// const dictionary = new Dictionary('Java', 'Number')
+// dictionary.add('Web-developer', 'this Software Enginner make a web-sites')
+// dictionary.add('game-developer', 'he make a games')
+// dictionary.remove('game-developer')
+// dictionary.showAllWords()
